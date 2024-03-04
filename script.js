d71b9a9d530521e86f91c8561a5bee26db4ac6cd
Maruf OVi <imgoinin.iamovi@gmail.com>
2024-03-05 01:23:23 +0600

    update project

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// script.js
+document.onreadystatechange = function () {
+  var state = document.readyState;
+  if (state == 'interactive') {
+      document.getElementById('sun').style.visibility="hidden";
+      document.getElementById('moon').style.visibility="hidden";
+      document.getElementById('toggleBtn').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+          document.getElementById('interactive');
+          document.getElementById('load').style.visibility="hidden";
+          document.getElementById('sun').style.visibility="visible";
+          document.getElementById('moon').style.visibility="visible";
+          document.getElementById('toggleBtn').style.visibility="visible";
+      },1000);
+  }
+}
+
+
 document.getElementById("toggleBtn").addEventListener("click", function() {
     var sun = document.getElementById("sun");
     var moon = document.getElementById("moon");
